6238c001b810d751c4142a24f5f4b4c6857f4ac3
Johanna Liza <johanna.herrera@al.infnet.edu.br>
2024-12-01 00:18:54 -0300

    Primeiro commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { NativeBaseProvider} from "native-base";
+
+function App() {
+  return (
+    <NativeBaseProvider>
+      <button>Olá</button>
+    </NativeBaseProvider>
+  );
+}
+
+export default App;
